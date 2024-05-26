4f32bc56dde6315ff36434fbdd88fe491193151f
Marco <marco10@live.at>
2024-05-26 18:50:10 +0200

    Added Checkbox Added Delete Button ListItems can be added and removed now ListItems can be toggled checked and unchecked now

--- index.js
+++ index.js
@@ -14,6 +14,31 @@ const addTask = () => {
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
 
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span)
+
         inputBox.value = "";
+        saveData();
     }
 }
+
+listContainer.addEventListener('click', e => {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+})
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
